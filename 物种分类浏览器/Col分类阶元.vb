a24194f591c63ea2fd36840c147a378569830e3b
锹甲阿尔法 <zhangtianfu0@126.com>
2019-03-11 10:16:21 +0800

    添加项目文件。

---
+++ 物种分类浏览器/Col分类阶元.vb
@@ -0,0 +1,19 @@
+﻿Public Class Col分类阶元
+    Private id As UInteger, name As String
+
+    Public ReadOnly Property Content As String
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Public ReadOnly Property ItemsSource As String
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+    Sub New(id As UInteger, name As String)
+        Me.id = id
+        Me.name = name
+    End Sub
+End Class
